11020a97c4f7f3dbff12f29c235d62eb123df769
David Ko <david.ko@velvetreactor.com>
2018-02-05 07:45:50 -0500

    Call out to Agent API

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 const cnsl = require('commander');
+const request = require('axios');
 
-cnsl.option('-s, --say [phrase]', 'Make cnsl speak');
-cnsl.parse(process.argv);
-
-if (cnsl.say) {
-  console.log('%s', cnsl.say);
+function Member(json) {
+  this.name = json.Name;
 }
+
+cnsl
+  .option('-A, --agent <cmd>', 'Call the Consul Agent API', async cmd => {
+    let url = `http://localhost:8500/v1/agent/${cmd}`;
+    let res = await request.get(url);
+    let apiMembers = res.data;
+    let members = apiMembers.map(member => new Member(member));
+    console.log(members);
+  });
+
+cnsl.parse(process.argv);
