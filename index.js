157afc33d1efdbf1d5e5c2d4d96fb3d4211ad566
David Ko <david.ko@pvtmethod.com>
2018-02-07 13:28:16 -0500

    Adds ConsulServer and Node classes

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ function Member(json) {
   this.name = json.Name;
 }
 
+function ConsulServer() {
+  this.type = 'consul_server';
+}
+
+function Node() {
+  this.type = 'node';
+}
+
 cnsl
   .option('-A, --agent <cmd>', 'Call the Consul Agent API', async cmd => {
     let url = `http://localhost:8500/v1/agent/${cmd}`;
     let res = await request.get(url);
     let apiMembers = res.data;
     let members = apiMembers.map(member => new Member(member));
-    console.log(members);
+    console.log(res);
   });
 
 cnsl.parse(process.argv);
